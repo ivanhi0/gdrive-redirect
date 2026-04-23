b62cd9a9132f623f82c8ace147fc844665076e81
ivanhi0 <id.bodrov@gmail.com>
2026-04-23 20:45:16 +0300

    Replace link

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 // hahaha I got you
 //determinate where user located
-const countrcode = document.getElementById('country_code');
-
-fetch('https://ipwho.is/')
-  .then(res => res.json())
-  .then(res => {
-    const countryCode = res.country_code;
-
-    // Redirect user based on his country location because YouTube bloked in Russia
-    if (countryCode == "RU") {
-      window.location.href = "https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb";
-      window.location.replace("https://rutube.ru/video/ac4ac2f35c35fe2dc78e9a66c48097cb");
-    } else {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }
-  });
+window.location.href = "https://drive.google.com/drive/folders/1RMJC6c1VuBI2tulG5WGPKlHoFOKF8brJ?usp=drive_link";
+window.location.replace("https://drive.google.com/drive/folders/1RMJC6c1VuBI2tulG5WGPKlHoFOKF8brJ?usp=drive_link");
