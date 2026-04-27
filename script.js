76027dde4120df492abf57c4f41a1a3b52f64e3b
ivanhi0 <idbod@ivanhi0.ru>
2026-04-27 22:37:00 +0300

    Replace link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // hahaha I got you
 //determinate where user located
-window.location.href = "https://drive.google.com/drive/folders/1RMJC6c1VuBI2tulG5WGPKlHoFOKF8brJ?usp=drive_link";
-window.location.replace("https://drive.google.com/drive/folders/1RMJC6c1VuBI2tulG5WGPKlHoFOKF8brJ?usp=drive_link");
+window.location.href = "https://drive.google.com/drive/folders/1t28iT6rSKvqrTX1imwgtSnvTvnoeez2f?usp=drive_link";
+window.location.replace("https://drive.google.com/drive/folders/1t28iT6rSKvqrTX1imwgtSnvTvnoeez2f?usp=drive_link");
