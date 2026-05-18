96e82537c74ca35f332b36d8d5f677a6d4e06f9f
ivanhi0 <idbod@ivanhi0.ru>
2026-05-18 22:59:47 +0300

    Update link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // hahaha I got you
 //determinate where user located
-window.location.href = "https://drive.google.com/drive/folders/1uWMQcRcvlSTFJrNgyIQCb2kNwXluOCBg?usp=drive_link";
-window.location.replace("https://drive.google.com/drive/folders/1uWMQcRcvlSTFJrNgyIQCb2kNwXluOCBg?usp=drive_link");
+window.location.href = "https://drive.google.com/drive/folders/1QUNjecmDZ7w6eK_x70zDvmJLnkp6f2Kx?usp=drive_link";
+window.location.replace("https://drive.google.com/drive/folders/1QUNjecmDZ7w6eK_x70zDvmJLnkp6f2Kx?usp=drive_link");
